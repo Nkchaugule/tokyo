1ea5bef1c03bc4a004862170abb0cee7b7b43067
Nkchaugule <chaugulenikhil650@gmail.com>
2021-07-05 14:28:51 +0530

    file added successfully

---
+++ op.java
@@ -0,0 +1,9 @@
+class op
+{
+	public static void main(String[] arg)
+	{
+		System.out.println("Hello Nikhil");
+		System.out.println("How are you !");
+		System.out.println("You have configured successfulluy");
+	}
+}
